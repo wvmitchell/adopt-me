4f18329c80296cb97465ad7081ea9ae096009ad3
Will Mitchell <wvmitchell@gmail.com>
2024-06-10 14:01:18 -0600

    checkpoint 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
 const Pet = function (props) {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -20,5 +23,5 @@ const App = function () {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
