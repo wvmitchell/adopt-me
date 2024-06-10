5ac33fb89a26b38a12b5402629196954e0df7b78
Will Mitchell <wvmitchell@gmail.com>
2024-06-10 11:37:11 -0600

    checkpoint 1 - no frills react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+const Pet = function(props) {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
+  ])
+}
+
+const App = function() {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {name: 'Luna', animal: 'Dog', breed: 'Havanese'}),
+    React.createElement(Pet, {name: 'Apollo', animal: 'Dog', breed: 'Mutt'}),
+    React.createElement(Pet, {name: 'Hudson', animal: 'Dog', breed: 'Lab'}),
+  ]);
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
