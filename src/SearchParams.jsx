8b4da673dba78225b34077a806122e77f40e3408
Will Mitchell <wvmitchell@gmail.com>
2024-06-11 10:27:51 -0600

    checkpoint 7 - component composition

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import useBreedList from "./useBreedList";
-import Pet from "./Pet";
+import Results from "./Results";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = function () {
@@ -75,14 +75,7 @@ const SearchParams = function () {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
