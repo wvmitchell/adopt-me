2a8a9a7e7c9f0f5f6f8db8f9b020716bd845f071
Will Mitchell <wvmitchell@gmail.com>
2024-06-12 14:48:56 -0600

    checkpoint 12 - modals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import Carosel from "./Carosel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [isSold, setIsSold] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -15,6 +20,11 @@ const Details = () => {
     );
   }
 
+  function handleButtonClick(e) {
+    setShowModal(false);
+    setIsSold(e.target.dataset.sold === "true");
+  }
+
   const pet = results.data.pets[0];
 
   return (
@@ -22,12 +32,37 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>
+          {isSold ? "Sell" : "Adopt"} {pet.name}
+        </button>
         <p>{pet.description}</p>
       </div>
       <Carosel images={pet.images} />
+      {showModal ? (
+        <Modal>
+          <div className="modal">
+            <h2>Would you like to adopt {pet.name}?</h2>
+            <div className="buttons">
+              <button data-sold="false" onClick={handleButtonClick}>
+                Yes
+              </button>
+              <button data-sold="true" onClick={handleButtonClick}>
+                No
+              </button>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   );
 };
 
-export default Details;
+const WrappedDetails = (props) => {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+};
+
+export default WrappedDetails;
